2616ce3d61aa727f151f5f4b00220262ab403db1
Vadduri Triveni <97962294+VadduriTriveni@users.noreply.github.com>
2025-01-31 21:28:31 +0530

    Update Q2.java

--- Top 50 Q&S/Q2.java
+++ Top 50 Q&S/Q2.java
@@ -1,55 +1,19 @@
 242. Valid Anagram
 ==============================================================================================
 class Solution {
+
     public boolean isAnagram(String s, String t) {
-      
-        if(s.length() != t.length()){
-            return false;
-        }
-        
-        int[] count = new int[26];
-        
-        for(int i=0; i<s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(count[i] != 0){
-                return false;
-            }
-        }
-        return true; 
-    }
-}
----------------------------------------------------------------------------------------------
-class Solution {
-    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
 
-         if (s.length() != t.length()) {
-            return false;
+        int[] store = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
         }
 
-        HashMap<Character,Integer> hs = new HashMap<>();
-        for(char ch : s.toCharArray())
-        {
-            hs.put(ch,hs.getOrDefault(ch, 0)+1);
-        }
+        for (int n : store) if (n != 0) return false;
 
-        for( char ch : t.toCharArray())
-        {
-            if(!hs.containsKey(ch))
-            {
-                return false;
-
-            }
-            hs.put(ch,hs.get(ch)-1);
-
-            if(hs.get(ch)<0)
-            {
-                return false;
-            }
-        }
-        
         return true;
     }
 }
