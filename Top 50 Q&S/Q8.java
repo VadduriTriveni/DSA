018d588da9d04823288f6e4aa6b81d3c79e42077
Vadduri Triveni <97962294+VadduriTriveni@users.noreply.github.com>
2025-01-31 21:34:45 +0530

    Update Q8.java

--- Top 50 Q&S/Q8.java
+++ Top 50 Q&S/Q8.java
@@ -1,37 +1,26 @@
 128. Longest Consecutive Sequence
 ========================================================================================
-  class Solution {
+class Solution {
     public int longestConsecutive(int[] nums) {
-        
-        if(nums.length ==0){
-            return 0;
-        }
-        
-        HashSet<Integer> numSet = new HashSet<>();
-        for(int i=0; i<nums.length; i++){
-            numSet.add(nums[i]);
-        }
-        
-        int longestSub = 1;
-        
-        for(int num:numSet){
-            if(numSet.contains(num-1)){
-                continue;
-            }
-            else{
-                int currentNum = num;
-                int currentSub = 1;
-                while(numSet.contains(currentNum+1)){
-                    currentNum++;
-                    currentSub++;
+       if (nums.length == 0) return 0;
+       HashSet<Integer> hs = new HashSet<>();
+       for(int num:nums) hs.add(num);
+       int longest =1;
+       for(int num: nums ){
+           //check if the num is the start of a sequence by checking if left exists
+           if(!hs.contains(num-1)){ // start of a sequence
+                int count =1;
+                while(hs.contains(num + 1)){ // check if hs contains next no.
+                    num++;
+                    count++;
                 }
+                longest = Math.max(longest, count);
                 
-                longestSub = Math.max(longestSub, currentSub);
-            }
-            
-            
-        }
-        return longestSub;
-        
+           }
+           if(longest > nums.length/2) break;
+
+       }
+       return longest;
     }
 }
+
