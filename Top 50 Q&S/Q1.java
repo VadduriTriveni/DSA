87bac54c159b2c8e2f36438cc63d10a1b3b7abb6
Vadduri Triveni <97962294+VadduriTriveni@users.noreply.github.com>
2025-01-31 21:27:23 +0530

    Update Q1.java

--- Top 50 Q&S/Q1.java
+++ Top 50 Q&S/Q1.java
@@ -1,17 +1,14 @@
 217. Contains Duplicate
 =========================================================================================
 class Solution {
+
     public boolean containsDuplicate(int[] nums) {
-        
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int i=0; i< nums.length; i++){
-            if(set.contains(nums[i])){
+        Set<Integer> uniques = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (uniques.contains(nums[i])) {
                 return true;
             }
-            else{
-                set.add(nums[i]);
-            }
+            uniques.add(nums[i]);
         }
         return false;
     }
